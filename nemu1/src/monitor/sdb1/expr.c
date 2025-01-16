ea5009efa27b349f9206b330968f192adf73d21f
tracer-ysyx <tracer@ysyx.org>
2025-01-16 09:48:44 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:48:44 up  9:57,  1 user,  load average: 0.06, 0.12, 0.16

---
+++ nemu1/src/monitor/sdb1/expr.c
@@ -0,0 +1,164 @@
+/***************************************************************************************
+* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#include <isa.h>
+
+/* We use the POSIX regex functions to process regular expressions.
+ * Type 'man regex' for more information about POSIX regex functions.
+ */
+#include <regex.h>
+#include <stdio.h>
+
+enum {
+  TK_NOTYPE = 256,
+  /* TODO: Add more token types */
+  TK_EQ,           // ==
+  TK_NE,           // !=
+  TK_NUMBER,       // 数字
+  TK_PLUS,         // +
+  TK_MINUS,        // -
+  TK_MULTIPLY,     // *
+  TK_DIVIDE,       // /
+  TK_MODULO,       // %
+  TK_LPAREN,       // (
+  TK_RPAREN        // )
+  /* 如果需要扩展，例如位运算符或逻辑运算符，可以在这里添加 */
+};
+
+
+
+static struct rule {
+  const char *regex;
+  int token_type;
+} rules[] = {
+
+  /* TODO: Add more rules.
+   * Pay attention to the precedence level of different rules.
+   */
+
+  {" +", TK_NOTYPE},    // spaces
+  {"\\+", '+'},         // plus
+  {"==", TK_EQ},        // equal
+  {"-", TK_MINUS},         // minus
+  {"\\*", TK_MULTIPLY},    // multiply
+  {"/", TK_DIVIDE},        // divide
+  {"%", TK_MODULO},        // modulo
+  {"!=", TK_NE},           // not equal
+  {"\\(", TK_LPAREN},      // left parenthesis
+  {"\\)", TK_RPAREN},      // right parenthesis
+  {"[0-9]+", TK_NUMBER},   // numbers
+  
+};
+
+#define NR_REGEX ARRLEN(rules)
+
+static regex_t re[NR_REGEX] = {};
+
+/* Rules are used for many times.
+ * Therefore we compile them only once before any usage.
+ */
+void init_regex() {      //正则表达式编译部分
+  int i;
+  char error_msg[128];
+  int ret;
+
+  for (i = 0; i < NR_REGEX; i ++) {
+    ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);    //通过 regcomp 函数编译 rules 数组中的每个正则表达式
+    if (ret != 0) {
+      regerror(ret, &re[i], error_msg, 128);
+      panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+    }
+  }
+}
+
+typedef struct token {
+  int type;
+  char str[32];
+} Token;
+
+static Token tokens[32] __attribute__((used)) = {}; //初始化结构体
+static int nr_token __attribute__((used))  = 0;   //初始化记录存储在 tokens 数组中的词法单元的数量。
+
+static bool make_token(char *e) {
+  int position = 0;
+  int i;
+  regmatch_t pmatch;
+
+  nr_token = 0;      //用于记录 tokens 数组中当前已经存储了多少个 token。
+
+  while (e[position] != '\0') {
+    /* Try all rules one by one. */
+    for (i = 0; i < NR_REGEX; i ++) {
+      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+        char *substr_start = e + position;
+        int substr_len = pmatch.rm_eo;
+
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+
+        position += substr_len;
+
+        /* TODO: Now a new token is recognized with rules[i]. Add codes
+         * to record the token in the array `tokens'. For certain types
+         * of tokens, some extra actions should be performed.
+         */
+
+        switch (rules[i].token_type) {
+           case TK_NOTYPE: // Ignore spaces
+                break;
+
+           case TK_NUMBER:
+             tokens[nr_token].type = TK_NUMBER;
+             if (substr_len < sizeof(tokens[nr_token].str)) {
+                strncpy(tokens[nr_token].str, substr_start, substr_len);
+                tokens[nr_token].str[substr_len] = '\0'; // Null-terminate
+              } else {
+                printf("Token too long\n");
+                return false;
+             }
+                nr_token++;    // 每次识别到一个 token，增加计数
+                break;
+          default:            //处理 无需额外处理逻辑的简单 token-这些符号都无需解析其值，仅需要记录其类型，供后续表达式求值使用。
+             //TODO();    
+             tokens[nr_token].type = rules[i].token_type;
+             nr_token++;
+             break;
+        }
+
+        break;
+      }
+    }
+
+    if (i == NR_REGEX) {
+      printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+word_t expr(char *e, bool *success) {
+  if (!make_token(e)) {
+    *success = false;
+    return 0;
+  }
+
+  /* TODO: Insert codes to evaluate the expression. */
+ // TODO();
+
+  return 0;
+}
