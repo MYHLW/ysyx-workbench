1bcebe77195be4d30c98db2a6955c45d669561ed
tracer-ysyx <tracer@ysyx.org>
2025-01-17 09:47:22 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:47:22 up 32 min,  1 user,  load average: 0.48, 0.25, 0.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -96,7 +96,10 @@ static int nr_token __attribute__((used))  = 0;
 
 
 static bool make_token(char *c) {   //c->e
-  char *e = strtok(c," ");    //按空格分割表达式
+  char *e = strtok(c, " ");    //按空格分割表达式
+   for (int i = 0; e[i] != '\0'; i++) {  // 直到遇到结束符'\0'
+        printf("%c", e[i]);  // 打印每个字符
+    }    
   int position = 0;
   int i;
   regmatch_t pmatch;
