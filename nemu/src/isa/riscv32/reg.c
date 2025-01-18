75a8ee38c0b4a950033c22ff0b9726c40895af1a
tracer-ysyx <tracer@ysyx.org>
2025-01-18 21:55:10 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:55:10 up 17:17,  1 user,  load average: 0.06, 0.07, 0.15

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -55,9 +55,11 @@ sp     0x00000002
 
 //I fix the module
 word_t isa_reg_str2val(const char *s, bool *success) {
-
+  if (s[0] == '$') {
+    s++;  // 跳过 '$' 符号
+  }
    for (int i = 0; i < 32; i++) {
-    if (s==regs[i]) {
+    if (strcmp(s, regs[i]) == 0) {
       *success = true;
       printf("The REG is %s",regs[i]);
       return cpu.gpr[i];
