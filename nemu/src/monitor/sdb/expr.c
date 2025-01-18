2be5c33f5f1dd763173b781666e7b234989d05f0
tracer-ysyx <tracer@ysyx.org>
2025-01-18 15:54:48 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:54:48 up 12:31,  1 user,  load average: 0.11, 0.08, 0.11

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -250,9 +250,10 @@ int find_main_op(int p, int q) {
       if (current_priority <= op_priority) {
           op_priority = current_priority;
           main_op = i;
-          printf("main_op(i) = %d",main_op);  //
+          
       }
     }
+  printf("main_op(i) = %d\n",main_op);  //
   return main_op;
 }
 
