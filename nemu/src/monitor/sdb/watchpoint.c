628f4b21185e2e44af2ba78838e349d7c273eaa3
tracer-ysyx <tracer@ysyx.org>
2025-01-19 21:07:22 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:07:22 up 23:33,  1 user,  load average: 0.33, 0.11, 0.03

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -99,8 +99,10 @@ WP* create_watchpoint(char *expr_str) {
   WP *wp = new_wp();  // 从监视点池申请一个新的监视点
   
   strncpy(wp->expr, expr_str, sizeof(wp->expr) - 1);  // 保存表达式
-  printf("1"); 
+  
   wp->expr[sizeof(wp->expr) - 1] = '\0';  // 确保字符串以NULL结尾
+  printf("1"); 
+  
   wp->value = expr(wp->expr, NULL);  // 计算表达式的初始值并存储
   printf("1");  
   
