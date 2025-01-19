f6de9d4b8551be972581f51fa81996446fb9a853
tracer-ysyx <tracer@ysyx.org>
2025-01-19 20:48:48 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:48:48 up 23:15,  1 user,  load average: 0.00, 0.00, 0.04

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -320,7 +320,7 @@ word_t expr(char *e, bool *success) {
   for (int i = 0; i <NR_REGEX; i++) {
 	if (tokens[i].type == TK_MULTIPLY && (i == 0 || tokens[i - 1].type == '('||tokens[i - 1].type==TK_EQ||tokens[i - 1].type==TK_NE)) {
         	tokens[i].type = TK_DEREF;
-    		printf(" i = %d\n",i);
+  //  		printf(" i = %d\n",i);
   }
 }
 
