a14d9594d69cbd7202efc4f023dde14835ee2fb9
tracer-ysyx <tracer@ysyx.org>
2025-01-20 21:08:25 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:08:25 up 1 day,  8:53,  1 user,  load average: 0.33, 0.16, 0.15

--- nemu/src/monitor/sdb/test.c
+++ nemu/src/monitor/sdb/test.c
@@ -8,7 +8,8 @@
 #include "sdb.h"
 
 int test(){
-	FILE *fp = fopen("/home/wang/ysyx-workbench/nemu/tools/gen-expr/input.txt","r");
+	//FILE *fp = fopen("/home/wang/ysyx-workbench/nemu/tools/gen-expr/input.txt","r");
+	FILE *fp = fopen("/home/wang/ysyx-workbench/nemu/tools/gen-expr/input","r");
 	if (fp == NULL){
 		assert(0);
 		}
