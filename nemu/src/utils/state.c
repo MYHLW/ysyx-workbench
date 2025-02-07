a05f689f918fa5f10b1ff3b057262055806d3818
tracer-ysyx <tracer@ysyx.org>
2025-02-07 21:24:59 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:59 up 1 day, 12:40,  1 user,  load average: 0.12, 0.10, 0.16

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -20,9 +20,5 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
-  //return !good;
-  if (1 == !good){
-  good = 0;}
-  return good;
-  
+  return !good;
 }
