c12f2e23d3e928792dbeb18f77c2ef061f6ba3a0
tracer-ysyx <tracer@ysyx.org>
2025-02-08 08:44:55 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:44:55 up 1 day, 17:57,  1 user,  load average: 0.09, 0.07, 0.17

--- nemu/src/monitor/sdb/test.c
+++ nemu/src/monitor/sdb/test.c
@@ -4,9 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include "sdb.h"
 #include "watchpoint.h"
-#include <common.h>
+#include "sdb.h"
 
 
 extern uint32_t expr(char *e, bool *success); //
