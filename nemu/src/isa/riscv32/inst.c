7d98401c93c62f8097ebcc3cfaecb897daa4cbed
tracer-ysyx <tracer@ysyx.org>
2025-05-24 15:03:54 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:03:54 up  2:55,  1 user,  load average: 0.29, 0.51, 0.34

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -50,6 +50,9 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+    
+    case TYPE_J:                   immJ(); break; // J-Type 不需要源寄存器
+
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }
