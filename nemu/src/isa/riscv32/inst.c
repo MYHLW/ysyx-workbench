6cb4ba1beef02d4b7ef8a35985cefa74d6ee65c9
tracer-ysyx <tracer@ysyx.org>
2025-05-24 15:02:10 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:02:10 up  2:53,  1 user,  load average: 0.71, 0.65, 0.36

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -24,7 +24,7 @@
 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, // none
+  TYPE_N, TYPE_J, // none
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
@@ -32,6 +32,14 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+// 在decode_operand函数中添加J型立即数处理
+#define immJ() do { \
+  uint32_t temp = (BITS(i, 31, 31) << 20) | \
+                  (BITS(i, 19, 12) << 12) | \
+                  (BITS(i, 20, 20) << 11) | \
+                  (BITS(i, 30, 21) << 1); \
+  *imm = SEXT(temp, 21); \
+} while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
@@ -64,7 +72,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
 
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = src1 + imm);
-  //INSTPAT("??????? ????? ????? ????? ????? 11011 11", jal  , J, s->dnpc = s->pc + imm; R(rd) = s->snpc;  ); //不能识别J指令
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, s->dnpc = s->pc + imm;       R(rd) = s->snpc; ); //添加J指令
   INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, s->dnpc = (src1 + imm) & ~1; R(rd) = s->snpc; );
   
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
