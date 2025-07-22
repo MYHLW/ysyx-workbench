9f525d72dc2112d0013496963148b1788dda5da7
tracer-ysyx <tracer@ysyx.org>
2025-07-22 16:36:50 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:50 up 13:48,  0 users,  load average: 0.08, 0.04, 0.09

--- npc/vsrc/mux_pc.v
+++ npc/vsrc/mux_pc.v
@@ -7,8 +7,7 @@ module muxpc #(
   parameter PC_INC = `CPU_WIDTH'h4
 )(
     input                        ena,         // 使能，复位后才开始更新 PC
-    input                        branch_eq,   // BEQ 有效
-    input                        branch_ne,   // BNE 有效
+    input                        branch       // 分支指令是否有效
     input                        zero,        // ALU zero 标志
     input                        jump,        // JAL/JALR 有效
     input  [`CPU_WIDTH-1:0]      imm,         // 已签扩的立即数
@@ -23,10 +22,10 @@ always @(*) begin
   end else if (jump) begin
     // 无条件跳转（JAL/JALR）
     next_pc = curr_pc + imm;
-  end else if (branch_eq && zero) begin
+  end else if (branch && zero) begin
     // BEQ 条件成立
     next_pc = curr_pc + imm;
-  end else if (branch_ne && ~zero) begin
+  end else if (branch && ~zero) begin
     // BNE 条件成立
     next_pc = curr_pc + imm;
   end else begin
