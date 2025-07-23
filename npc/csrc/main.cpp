6d2e49ed098fca270eeb6f4284e4b3e366e26647
tracer-ysyx <tracer@ysyx.org>
2025-07-23 15:24:31 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:24:31 up  1:28,  1 user,  load average: 0.48, 0.24, 0.23

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -41,7 +41,7 @@ int main(){
   tfp->open("Vysyx_25020059.vcd"); // 打开VCD文件
 
   reset(1); // 重置1个周期
-  for (int i = 0; i < num_instructions + 1; i++) {
+  for (int i = 0; i < num_instructions + 2; i++) {
     dut.inst = pmem_read(mem, dut.curr_pc); // 从内存读取指令
     single_cycle(); // 执行一个周期
     tfp->dump(contextp->time()); // 转储当前时间的跟踪数据
