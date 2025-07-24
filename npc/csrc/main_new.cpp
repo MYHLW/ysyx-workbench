32d8f061ca85b5abc0b23e7d33dbc86eeccf707f
tracer-ysyx <tracer@ysyx.org>
2025-07-24 17:08:56 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:08:56 up 13:05,  1 user,  load average: 0.07, 0.13, 0.17

--- npc/csrc/main_new.cpp
+++ npc/csrc/main_new.cpp
@@ -57,12 +57,15 @@ static bool sim_done = false;
 
 // DPI 回调
 extern "C" void npc_trap(int code) {
+  static bool triggered = false;
+  if (triggered) return;  // 已打印过，直接丢弃
+  triggered = true;
   if (code == 0) {
     printf("[NPC] GOOD TRAP: program exited successfully.\n");
   } else {
     printf("[NPC] BAD TRAP: program failed (code=%d).\n", code);
   }
-  sim_done = true;      // 仅置标志，不直接 exit()
+  sim_done = true;
 }
 
 int main(int argc, char **argv) {
