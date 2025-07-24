d74b15cb7bce99a5bc751e4a26a822f837dcac32
tracer-ysyx <tracer@ysyx.org>
2025-07-24 10:41:20 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:41:20 up  7:17,  1 user,  load average: 0.37, 0.31, 0.24

--- npc/csrc/main_new.cpp
+++ npc/csrc/main_new.cpp
@@ -46,9 +46,9 @@ void single_cycle() {
 
 // 复位 n 个周期
 static void reset(int n) {
-    dut.rst = 0;
-    while (n-- > 0) single_cycle();
     dut.rst = 1;
+    while (n-- > 0) single_cycle();
+    dut.rst = 0;
 }
 
 // ============ DPI‑C: ebreak 触发退出 ==============
