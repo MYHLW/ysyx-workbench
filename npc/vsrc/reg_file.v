08b823d8a604cb449579a60447a6c57715a6c228
tracer-ysyx <tracer@ysyx.org>
2025-07-24 14:17:49 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:17:49 up 10:14,  1 user,  load average: 0.03, 0.21, 0.29

--- npc/vsrc/reg_file.v
+++ npc/vsrc/reg_file.v
@@ -11,7 +11,7 @@ module reg_file (
     input       [`REG_ADDR_WIDTH-1:0]   reg1_raddr, // Register read address 1
     input       [`REG_ADDR_WIDTH-1:0]   reg2_raddr, // Register read address 2
     output reg  [`CPU_WIDTH-1:0]        reg1_rdata, // Register read data
-    output reg  [`CPU_WIDTH-1:0]        reg2_rdata // Register read data
+    output reg  [`CPU_WIDTH-1:0]        reg2_rdata, // Register read data
     output reg [`CPU_WIDTH-1:0]        reg_f [`REG_DATA_DEPTH-1:0] // Register file array
 );
 
