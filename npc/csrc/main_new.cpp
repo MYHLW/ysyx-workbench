69336a64b6e4eabc6549a58fa8964c67da3b0d2c
tracer-ysyx <tracer@ysyx.org>
2025-07-24 10:43:06 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:43:06 up  7:19,  1 user,  load average: 0.08, 0.23, 0.21

--- npc/csrc/main_new.cpp
+++ npc/csrc/main_new.cpp
@@ -82,6 +82,8 @@ int main(int argc, char **argv) {
 
     // 初始化时钟与复位
     dut.clk = 0;
+    dut.rst = 1; // 初始复位
+    dut.eval(); // 评估初始状态
     reset(2);
 
     // 仿真主循环，直到 ebreak 调用 npc_trap 退出进程
