c0ba71d35bb1a697ea1775434698767b53174713
tracer-ysyx <tracer@ysyx.org>
2025-07-24 10:45:26 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:45:26 up  7:22,  1 user,  load average: 0.58, 0.36, 0.26

--- npc/csrc/main_new.cpp
+++ npc/csrc/main_new.cpp
@@ -47,7 +47,7 @@ void single_cycle() {
 // 复位 n 个周期
 static void reset(int n) {
     dut.rst = 0; dut.eval();
-   // while (n-- > 0) single_cycle();
+    while (n-- > 0) single_cycle();
     dut.rst = 1; dut.eval();
 }
 
@@ -82,9 +82,9 @@ int main(int argc, char **argv) {
 
     // 初始化时钟与复位
     dut.clk = 0;
+    reset(2);
     dut.rst = 1; // 初始复位
     dut.eval(); // 评估初始状态
-    reset(2);
 
     // 仿真主循环，直到 ebreak 调用 npc_trap 退出进程
     while (true) {
