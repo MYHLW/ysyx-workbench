aeec8de2d5e7e0901c08114f30d001be6a36dc73
tracer-ysyx <tracer@ysyx.org>
2025-07-24 16:21:13 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:21:13 up 12:18,  1 user,  load average: 0.00, 0.13, 0.15

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -18,14 +18,22 @@ MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here
 CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=\""$(MAINARGS_PLACEHOLDER)"\"
 
 insert-arg: image
-	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) "$(MAINARGS_PLACEHOLDER)" "$(mainargs)"
+	@python3 $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) "$(MAINARGS_PLACEHOLDER)" "$(mainargs)"
 
 image: image-dep
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
-run: insert-arg
-	echo "TODO: add command here to run simulation"
+# 指向绝对的 NPC 目录
+NPC_DIR := /home/wang/ysyx-workbench/npc
 
-.PHONY: insert-arg
+# AM 镜像文件
+AM_IMAGE := $(IMAGE).bin
+
+.PHONY: run
+run: image
+	@echo "==> Copying AM image to NPC dir"
+	@cp $(AM_IMAGE) $(NPC_DIR)/program/
+	@echo "==> Launching NPC sim"
+	@$(MAKE) -C $(NPC_DIR) sim2 IMG=$(AM_IMAGE)
