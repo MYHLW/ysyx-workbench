1b420cf3e3b2ef6f55d28b2ae1cc7a0de61b5ae5
tracer-ysyx <tracer@ysyx.org>
2025-07-24 17:45:19 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:45:19 up 13:42,  1 user,  load average: 0.01, 0.15, 0.20

--- npc/vsrc/ctrl.v
+++ npc/vsrc/ctrl.v
@@ -29,6 +29,7 @@ wire [`REG_ADDR_WIDTH-1:0] rd   = inst[`REG_ADDR_WIDTH+`RD_BASE-1:`RD_BASE];
 wire [`REG_ADDR_WIDTH-1:0] rs1  = inst[`REG_ADDR_WIDTH+`RS1_BASE-1:`RS1_BASE]; 
 wire [`REG_ADDR_WIDTH-1:0] rs2  = inst[`REG_ADDR_WIDTH+`RS2_BASE-1:`RS2_BASE]; 
 wire [11:0]             imm12   = inst[31:20];  // 用于 ebreak 指令的高 12 位
+wire [31:0]                code = a0; 
 
 always @(*) begin
     branch      = 1'b0;
@@ -44,7 +45,6 @@ always @(*) begin
 
     // ebreak 检测：SYSTEM opcode + funct3==0 + imm12==1
     if (opcode ==  7'b1110011 && funct3 == 3'b000 && imm12 == 12'h001) begin
-        code = a0; // 将 a0 作为 trap code
         npc_trap(code);
     end
     
