f6512aaba240647dfbe425ee645b26d51170e62d
tracer-ysyx <tracer@ysyx.org>
2025-07-25 14:41:36 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:41:36 up  1:05,  1 user,  load average: 1.24, 0.90, 0.49

--- npc/obj_dir/Vysyx_25020059_top.mk
+++ npc/obj_dir/Vysyx_25020059_top.mk
@@ -44,6 +44,8 @@ VM_USER_LDLIBS = \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
+	cli \
+	main \
 	main_new \
 
 # User .cpp directories (from .cpp's on Verilator command line)
@@ -61,6 +63,10 @@ include $(VERILATOR_ROOT)/include/verilated.mk
 ### Executable rules... (from --exe)
 VPATH += $(VM_USER_DIR)
 
+cli.o: ./csrc/cli.cpp 
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST)  -c -o $@ $<
+main.o: ./csrc/main.cpp 
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST)  -c -o $@ $<
 main_new.o: ./csrc/main_new.cpp 
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST)  -c -o $@ $<
 
