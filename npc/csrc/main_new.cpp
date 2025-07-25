f76c9e31859a6b5d7bb4d0a36d865a2d94d2c624
tracer-ysyx <tracer@ysyx.org>
2025-07-25 15:17:08 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:17:08 up  1:40,  1 user,  load average: 0.68, 0.25, 0.16

--- npc/csrc/main_new.cpp
+++ npc/csrc/main_new.cpp
@@ -21,11 +21,11 @@ uint8_t*          memory    = nullptr;
 #define MEM_BASE    0x80000000U
 #define MEM_SIZE    (8 * 1024 * 1024)  // 8MB
 
-// 从仿真内存读取指令
-uint32_t pmem_read(uint32_t vaddr) {
-    uint32_t off = vaddr - MEM_BASE;  // 转换为偏移
-    return *(uint32_t*)(memory + off);
-}
+// // 从仿真内存读取指令
+// uint32_t pmem_read(uint32_t vaddr) {
+//     uint32_t off = vaddr - MEM_BASE;  // 转换为偏移
+//     return *(uint32_t*)(memory + off);
+// }
 
 // 加载程序镜像
 void load_image(const char* filename) {
