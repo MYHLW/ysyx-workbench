564b452c53d4956c0a1b1b3a3358cc1264a2b808
tracer-ysyx <tracer@ysyx.org>
2025-07-27 21:45:28 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:28 up 11 min,  1 user,  load average: 0.22, 0.69, 0.58

--- npc/vsrc/pc_reg.v
+++ npc/vsrc/pc_reg.v
@@ -11,7 +11,7 @@ module pc_reg (
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         ena <= 0; // Reset enable signal
-        curr_pc <= 32'h0000_0000; // Reset current PC to 0
+        curr_pc <= 32'h8000_0000; // Reset current PC to 0
     end else begin
         ena <= 1; // Enable signal is set
         curr_pc <= next_pc; 
