be0ce81de1fb98884ee7111570c44e6fb8fd98e1
tracer-ysyx <tracer@ysyx.org>
2025-07-28 19:01:58 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:01:58 up  8:42,  1 user,  load average: 0.22, 0.26, 0.29

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -12,6 +12,7 @@ CFLAGS   += -fdata-sections -ffunction-sections
 LDFLAGS  += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS  += --gc-sections -e _start
+NPCFLAGS += -b
 
 CFLAGS   += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I $(NPC_HOME)/csrc/include
