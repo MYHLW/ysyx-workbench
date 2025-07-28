d8a79e6f5dd5319eb950cd674b9c6433b76e151e
tracer-ysyx <tracer@ysyx.org>
2025-07-28 10:37:19 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:37:19 up  1:46,  1 user,  load average: 0.24, 0.60, 0.38

--- npc/vsrc/reg_file.v
+++ npc/vsrc/reg_file.v
@@ -23,7 +23,7 @@ module reg_file (
 //         reg_f[reg_waddr] <= reg_wdata; // Write data to register
 //     end
 // end
-always @(posedge clk or negedge rst_n) begin
+always @(negedge clk or negedge rst_n) begin
     if (!rst_n) begin
         reg_f[reg_waddr] <= 0; // 原来的代码没有正确处理异步复位的逻辑
     end else if (reg_wen && (reg_waddr != 0)) begin
