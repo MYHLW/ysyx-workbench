b3bb80ea9b9a2c36327e9a86aecf333dc0918936
tracer-ysyx <tracer@ysyx.org>
2025-07-28 13:41:07 +0800

    > sim RTL ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:41:07 up  3:22,  1 user,  load average: 0.19, 0.09, 0.10

--- npc/csrc/main_new.cpp
+++ npc/csrc/main_new.cpp
@@ -89,16 +89,15 @@ void single_cycle() {
     dut.clk = 0;
     dut.eval();
     tfp->dump(ctx->time());
-    ctx->timeInc(1);
+    ctx->timeInc(2);
    // dut.inst = pmem_read(dut.curr_pc);
     
     dut.clk = 1;
     dut.eval();    
     tfp->dump(ctx->time());
     ctx->timeInc(1);
-
+    
     dut.inst = pmem_read(dut.curr_pc,1);
-
     sim_cycle++;
 }
 
