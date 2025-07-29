f67cee5e6e9b52fd0ab13cdde402ac83e95c796b
tracer-ysyx <tracer@ysyx.org>
2025-07-29 10:07:48 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:07:48 up 41 min,  0 users,  load average: 1.52, 0.77, 0.39

--- nemu/src/utils/mtrace.c
+++ nemu/src/utils/mtrace.c
@@ -1,3 +1,4 @@
+#ifdef CONFIG_MTRACE
 #include <common.h>
 
 
@@ -8,3 +9,4 @@ void display_pread(paddr_t addr, int len) {
 void display_pwrite(paddr_t addr, int len, word_t data) {
   printf("mtrace: write memory from 0x%08x, %d bytes, data: 0x%08x\n", addr, len, data);
 }
+#endif
