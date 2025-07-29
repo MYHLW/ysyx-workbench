6eda239492cc4d53e5594ae548203268d086f7e5
tracer-ysyx <tracer@ysyx.org>
2025-07-29 10:23:25 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:23:25 up 57 min,  0 users,  load average: 0.00, 0.07, 0.17

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -3,9 +3,11 @@
 
 void __am_timer_init() {
 }
-
+//这个函数的功能是获取系统启动到当前的累计运行时间（以微秒为单位），并将结果存储在传入的AM_TIMER_UPTIME_T结构体指针中。
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uint64_t low = inl(RTC_ADDR);
+  uint64_t high = inl(RTC_ADDR + 4);
+  uptime->us = (high << 32) | low; // 读取RTC寄存器的低位和高位，合并为一个64位的时间戳
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
