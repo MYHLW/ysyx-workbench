470170fe9c750d90b1cace6677ebfa5c3a94eb46
tracer-ysyx <tracer@ysyx.org>
2025-07-31 15:44:50 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:44:50 up  7:34,  1 user,  load average: 0.33, 0.11, 0.16

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -9,20 +9,14 @@ extern char _pmem_start;
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
-#define MAINARGS_MAX_LEN 128
-static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
+static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
 
 void putch(char ch) {
 }
 
 void halt(int code) {
-  // 将 code 放到 a0
-  asm volatile("mv a0, %0" :: "r"(code));
-  // 触发 ebreak，NPC 会捕获并退出
-  asm volatile("ebreak");
-  while(1);
+  while (1);
 }
-
 
 void _trm_init() {
   int ret = main(mainargs);
