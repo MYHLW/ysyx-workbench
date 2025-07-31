cd9b975fecad3be86f3659c383e366ddd0c31c8c
tracer-ysyx <tracer@ysyx.org>
2025-07-31 19:47:46 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:47:46 up 11:18,  1 user,  load average: 0.63, 0.70, 0.41

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,8 +5,8 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  printf("1\n");
   uint32_t key = inl(KBD_ADDR);
   kbd->keydown = key & KEYDOWN_MASK ? true : false;// 用位掩码判断按键是否按下
   kbd->keycode = key & ~KEYDOWN_MASK;//用按位与运算把 key 的第 16 位清零，仅保留低 15 位的键值部分。
+  printf("Key %s: %d\n", kbd->keydown ? "down" : "up", kbd->keycode);
 }
