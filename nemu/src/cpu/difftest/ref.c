bc0c61fb2580f53f251a942c1a18a4bbc4124850
tracer-ysyx <tracer@ysyx.org>
2025-08-11 11:28:16 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:28:16 up  2:07,  1 user,  load average: 0.23, 0.19, 0.25

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -18,7 +18,7 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
-void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
     memcpy(guest_to_host(addr), buf, n);
   } else {
@@ -26,7 +26,7 @@ void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   }
 }
 
-void difftest_regcpy(void *dut, bool direction) {
+__EXPORT void difftest_regcpy(void *dut, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
     memcpy(&cpu, dut, DIFFTEST_REG_SIZE);
   } else {
@@ -34,12 +34,12 @@ void difftest_regcpy(void *dut, bool direction) {
   }
 }
 
-void difftest_exec(uint64_t n) {
+__EXPORT void difftest_exec(uint64_t n) {
   cpu_exec(n);
 }
 
-void difftest_raise_intr(word_t NO) {
-  //isa_raise_intr(NO, cpu.pc);
+__EXPORT void difftest_raise_intr(word_t NO) {
+  assert(0);
 }
 
 __EXPORT void difftest_init(int port) {
