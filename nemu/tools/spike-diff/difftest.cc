a25b9380dd4c1e5c5ba35673ed3fbff4374dc866
tracer-ysyx <tracer@ysyx.org>
2025-08-17 20:08:12 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:08:12 up  8:34,  0 users,  load average: 1.01, 1.11, 1.08

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -52,6 +52,7 @@ static state_t *state = NULL;
 void sim_t::diff_init(int port) {
   p = get_core("0");
   state = p->get_state();
+  p->put_csr(0x300, 0x1800);
 }
 
 void sim_t::diff_step(uint64_t n) {
@@ -144,9 +145,9 @@ __EXPORT void difftest_init(int port) {
       NULL,
       true);
   s->diff_init(port);
-  if (state && state->mstatus) {
-    state->mstatus->write((reg_t)0x1800);
-  }
+  // if (state && state->mstatus) {
+  //   state->mstatus->write((reg_t)0x1800);
+  // }
 }
 
 __EXPORT void difftest_raise_intr(uint64_t NO) {
