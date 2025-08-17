6d483753dc6c69030876e6d711754c174302c43c
tracer-ysyx <tracer@ysyx.org>
2025-08-17 18:59:41 +0800

    > compile NEMU ysyx_22040000 wanghailin Linux wang-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:59:41 up  7:26,  0 users,  load average: 0.38, 0.37, 0.26

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -33,32 +33,16 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* 1) 保存 mepc / mcause */
   cpu.mepc = epc;
   cpu.mcause = NO; /* NO 应当是完整 mcause 编码（含 interrupt 位） */
+  cpu.mstatus =0x1800; 
   #ifdef CONFIG_ETRACE
     printf("\n[etrace] Trap! mcause = 0x%x, mepc = 0x%x\n", cpu.mcause, cpu.mepc);
   #endif
+  return cpu.mtvec; /* 2) 返回 mtvec */
+}
 
-  /* 2) MPIE <- MIE ; MIE <- 0 */
-  if (cpu.mstatus & MSTATUS_MIE) cpu.mstatus |= MSTATUS_MPIE;
-  else                           cpu.mstatus &= ~MSTATUS_MPIE;
-  cpu.mstatus &= ~MSTATUS_MIE;
-
-  cpu.mstatus = (cpu.mstatus & ~MSTATUS_MPP_MASK) | ((word_t)PRV_M << 11);
-
-  /* 4) 根据 mtvec.mode 计算 trap 入口 */
-  word_t mtvec = cpu.mtvec;
-  word_t mode = mtvec & 0x3;        /* low 2 bits are mode */
-  word_t base = mtvec & ~((word_t)0x3);
-  word_t trap_pc;
-  if (mode == 1) { /* VECTORED: base + 4 * cause_index (cause low bits) */
-    /* 去掉 mcause 的 interrupt 高位来取索引 */
-    int xlen = sizeof(word_t) * 8;
-    word_t cause_index = cpu.mcause & (~((word_t)1 << (xlen-1)));
-    trap_pc = base + 4 * cause_index;
-  } else { /* DIRECT or reserved -> base */
-    trap_pc = base;
-  }
-
-  return trap_pc;
+word_t isa_mret() {
+  cpu.mstatus = 0x1800; 
+  return cpu.mepc; /* 恢复 PC */
 }
 
 word_t isa_query_intr() {
